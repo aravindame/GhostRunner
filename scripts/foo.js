e5847a9e6cde98080db6eb60d71010b2fc01df02
aravindame <aravinda.mailbox@gmail.com>
2016-09-30 20:36:54 +0530

    MinerJs

---
+++ scripts/foo.js
@@ -0,0 +1,12 @@
+var a = function (){
+  console.log("a");
+};
+
+var b = function(){
+  console.log("B");
+}
+
+module.exports = {
+  a : a,
+  b : b;
+};
---
+++ scripts/test.js
@@ -0,0 +1,28 @@
+/*
+This file is part of the MinnerJS project by Aravinda Meewalaarachchi.
+
+Copyright (C) 2016 Aravinda Meewalaarachchi <aravinda.mailbox@gmail.com>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+function main(){
+wait(2000);
+if(isElementPresent('id','btn'))
+    minerClick('xpath','//button[@id=\'btn\']');
+}
+
+$.getScript("http://127.0.0.1/file/Test/connectors/miner.js",main);
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Its works";
+}
