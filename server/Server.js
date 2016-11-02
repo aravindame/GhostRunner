82f333f5e698c42d14780e0167998fa5e6de843d
aravindame <aravinda.mailbox@gmail.com>
2016-11-02 15:07:23 +0530

    Server Updated

--- server/Server.js
+++ server/Server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer();
 server.on('request',function(req, res){
    res.writeHead(200,{'Content-Type' : 'text/plain'});
-   res.end('its works');
+   res.end('GhostRunner Server');
 })
 server.listen(3000);
 console.log('Server running at http://localhost:3000/');
